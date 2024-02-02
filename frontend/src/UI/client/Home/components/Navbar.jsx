0602797b019428d99e65981ebc26e92acd87ff69
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2024-02-02 22:24:07 +0545

    Designing Navbar

--- frontend/src/UI/client/Home/components/Navbar.jsx
+++ frontend/src/UI/client/Home/components/Navbar.jsx
@@ -1,26 +1,47 @@
 import React from 'react'
+import styled from 'styled-components';
+import "../styles/Navbar.css"
+import {Button} from '@mui/material'
 
+const Nav = styled.div.attrs({ className: 'navbar' })`
+    display: flex;
+    justify-content: space-between;
+    /* width: 100%; */
+    height: 100%;
+    margin-top: 10px;
+    background-color: red;
+`;
+
+
+const NavLink = styled.div.attrs({ className: 'navlink' })`
+    display: flex;
+    justify-content: space-between;
+    width: 30%;
+    margin-right: 10%;
+    align-items: center;
+    background-color: green;
+`;
 
 
 const Navbar = () => {
-  return (
-    <div class = 'navbar'>
-        <div>
-            <img src="./images/logo.png" alt="" />
-        </div>
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Blog</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-        <div>
-            <button>Appointment</button>
-        </div>
-    </div>
-  )
+    return (
+        <Nav className='navbar'>
+            <div>
+                <img src="./images/logo.png" alt="" />
+            </div>
+            <NavLink className='navlink'>
+                <div>
+                    <span>Home</span>
+                    <span>About Us</span>
+                    <span>Blog</span>
+                    <span>Contact</span>
+                </div>
+                <div>
+                    <Button variant="outlined">Appointment</Button>
+                </div>
+            </NavLink>
+        </Nav>
+    )
 }
 
 export default Navbar
