7148e5d035b320079a4da643d1ec16758cf29a0f
Bishal Lamichhane <60748756+Bishal75bct029@users.noreply.github.com>
2024-02-12 10:14:07 +0545

    Update db.js
    reminder

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//goru maile yo change office bata gareko ho. taile const uri ma dbname rakh hai tyo narakhera error aako raixa jasto lagyo. dbname chai last ma mongodb.net/dbname teha hunxa hai
 const uri = 'mongodb+srv://sandeepkhadka49356:Sandeep@123@cluster0.5gabacp.mongodb.net/';
 console.log(uri,"uri");
 
